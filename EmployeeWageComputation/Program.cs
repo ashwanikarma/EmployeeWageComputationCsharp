72dd33dd8bb1396a3cc7410eb3e730dfcba66eaa
ashwanikarma <ashwanikarma9@gmail.com>
2023-01-04 16:27:14 +0530

    Added Part Time employee to the Project

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -9,16 +9,21 @@
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0,2);
+            int empCheck = random.Next(0,3);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                Console.WriteLine("Employee is Present and Working FullTime");
                 empHrs = 8;
             }
-            else
+            else if (empCheck == 0)
             {
                 Console.WriteLine("Employee is Absent");
                 empHrs = 0;
+            }
+            else
+            {
+                Console.WriteLine("Employee is Working PartTime");
+                empHrs = 4;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Employee Wage : "+empWage);
