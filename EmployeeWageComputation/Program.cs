2d202d63e273df6aa2029f4e00e3bbe60fcc274f
ashwanikarma <ashwanikarma9@gmail.com>
2023-01-09 16:14:18 +0530

    [Ashwani] - Added constructor to initialise value and save total wage by company

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -5,10 +5,23 @@
 
         public const int IS_FULL_TIME = 2;
         public const int IS_PART_TIME = 1;
-        public static void computeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+
+        private string company;
+        private int empRatePerHour;
+        private int numOfWorkingDays;
+        private int maxHoursPerMonth;
+        private int totalEmpWage;
+
+        public Program(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
         {
-            int empHrs = 0, empTotalWorkingDays=1;
-            int totalEmpWage = 0;  
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays= numOfWorkingDays;
+            this.maxHoursPerMonth= maxHoursPerMonth;   
+        }
+        public  void computeEmpWage()
+        {
+            int empHrs = 0, empTotalWorkingDays=1;           
             int totalHrs = 0;
             while (totalHrs <= maxHoursPerMonth && empTotalWorkingDays <= numOfWorkingDays)
             {
@@ -33,15 +46,24 @@
                 Console.WriteLine("Day : "+empTotalWorkingDays+" Emp hrs : "+empHrs);
                 empTotalWorkingDays++;                
             }
-            totalEmpWage = totalHrs * empRatePerHour;
+            this.totalEmpWage = totalHrs * this.empRatePerHour;
             Console.WriteLine("Total Hours worked : " + totalHrs );
-            Console.WriteLine("Total Employee Wage for company : " +company + " is : "+totalEmpWage);
+          //  Console.WriteLine("Total Employee Wage for company : " +company + " is : "+totalEmpWage);
           
+        }
+
+        public void toString()
+        {
+            Console.WriteLine("Total Employee Wage for company : " + this.company + " is : " + this.totalEmpWage);
         }
         static void Main(string[] args)
         {
-            computeEmpWage("Dmart", 20, 10, 80);
-            computeEmpWage("Bigbazaar", 15, 15, 100);
+            Program dMart = new Program("Dmart", 20, 10, 80);
+            Program bigBazaar = new Program("Bigbazaar", 15, 15, 100);
+            dMart.computeEmpWage();
+            dMart.toString();
+            bigBazaar.computeEmpWage();
+            bigBazaar.toString();
         }
     }
 }
